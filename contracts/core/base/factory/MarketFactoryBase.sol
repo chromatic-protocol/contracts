1d8c57483531f91f71a38c685dac9e9e18bf2c58
daniel-fi <daniel.fi@quarkonix.com>
2023-04-26 15:05:40 +0900

    fix: revert message Fixes #44

--- contracts/core/base/factory/MarketFactoryBase.sol
+++ contracts/core/base/factory/MarketFactoryBase.sol
@@ -1,19 +1,13 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity >=0.8.0 <0.9.0;
 
-// import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
-// import {InterestRateLib} from "@usum/core/libraries/InterestRate.sol";
-// import {ISettlementTokenRegistry} from "@usum/core/interfaces/ISettlementTokenRegistry.sol";
 import {IUSUMMarketFactory} from "@usum/core/interfaces/IUSUMMarketFactory.sol";
-
-// import {InterestRateLib, Record} from "@usum/core/libraries/InterestRate.sol";
-// import {Registry} from "@usum/core/libraries/SettlementToken.sol";
 
 abstract contract MarketFactoryBase is IUSUMMarketFactory {
     address public override dao;
 
     modifier onlyDao() {
-        require(msg.sender == dao);
+        require(msg.sender == dao, "only DAO can access");
         _;
     }
 
