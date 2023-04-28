60ad781cd4f0fde4d8b17204051a3191da8ba0c3
daniel-fi <daniel.fi@quarkonix.com>
2023-04-28 15:08:55 +0900

    fix: deploy `USUMLiquidator` Fixes #54

--- deploy/1_deploy_core.ts
+++ deploy/1_deploy_core.ts
@@ -1,36 +1,41 @@
-import chalk from "chalk";
-import type { DeployFunction } from "hardhat-deploy/types";
-import type { HardhatRuntimeEnvironment } from "hardhat/types";
+import { GELATO_ADDRESSES } from "@gelatonetwork/automate-sdk"
+import chalk from "chalk"
+import type { DeployFunction } from "hardhat-deploy/types"
+import type { HardhatRuntimeEnvironment } from "hardhat/types"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts, network, ethers } = hre;
-  const { deploy } = deployments;
-  const { deployer } = await getNamedAccounts();
+  const { deployments, getNamedAccounts, network, ethers } = hre
+  const { deploy } = deployments
+  const { deployer } = await getNamedAccounts()
 
-  console.log(chalk.yellow(`✨ Deploying... to ${network.name}`));
+  console.log(chalk.yellow(`✨ Deploying... to ${network.name}`))
 
+  let automateAddress =
+    GELATO_ADDRESSES[
+      network.name === "anvil" ? 421613 : network.config.chainId!
+    ].automate
   // FIXME
-  let opsAddress = ethers.constants.AddressZero;
-  let keeperFeePayer = ethers.constants.AddressZero;
+  let keeperFeePayer = ethers.constants.AddressZero
 
   let { address: oracleRegistry } = await deploy("OracleRegistry", {
     from: deployer,
-  });
+  })
+
+  let { address: liquidator } = await deploy("USUMLiquidator", {
+    from: deployer,
+    args: [automateAddress],
+  })
 
   // // FIXME: need ops mock or real one
-  // let { address: liquidator } = await deploy("USUMLiquidator", {
-  //   from: deployer,
-  //   args: [opsAddress],
-  // });
-
   // await deploy("USUMMarketFactory", {
   //   from: deployer,
   //   args: [oracleRegistry, liquidator, keeperFeePayer],
   // });
-  console.log(chalk.yellow(`✨ oracleRegistry: ${oracleRegistry}`));
-};
+  console.log(chalk.yellow(`✨ OracleRegistry: ${oracleRegistry}`))
+  console.log(chalk.yellow(`✨ USUMLiquidator: ${liquidator}`))
+}
 
-export default func;
+export default func
 
-func.id = "deploy_core"; // id required to prevent reexecution
-func.tags = ["core"];
+func.id = "deploy_core" // id required to prevent reexecution
+func.tags = ["core"]
