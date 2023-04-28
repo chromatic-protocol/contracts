371acb35d24ca522e3718016fc9480b9ad347d14
daniel-fi <daniel.fi@quarkonix.com>
2023-04-28 15:28:44 +0900

    fix: deploy `USUMMarketFactory` Fixes #54

--- deploy/1_deploy_core.ts
+++ deploy/1_deploy_core.ts
@@ -1,33 +1,84 @@
 import { GELATO_ADDRESSES } from "@gelatonetwork/automate-sdk"
+import { SWAP_ROUTER_02_ADDRESSES, WETH9 } from "@uniswap/smart-order-router"
 import chalk from "chalk"
 import type { DeployFunction } from "hardhat-deploy/types"
 import type { HardhatRuntimeEnvironment } from "hardhat/types"
 
+const ARB_GOERLI_SWAP_ROUTER_ADDRESS =
+  "0xF1596041557707B1bC0b3ffB34346c1D9Ce94E86"
+
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts, network, ethers } = hre
+  const { config, deployments, getNamedAccounts, network, ethers } = hre
   const { deploy } = deployments
   const { deployer } = await getNamedAccounts()
 
+  const echainId =
+    network.name === "anvil"
+      ? config.networks.arbitrum_one_goerli.chainId!
+      : network.config.chainId!
+
   console.log(chalk.yellow(`✨ Deploying... to ${network.name}`))
 
-  let automateAddress =
-    GELATO_ADDRESSES[
-      network.name === "anvil" ? 421613 : network.config.chainId!
-    ].automate
-  // FIXME
-  let keeperFeePayer = ethers.constants.AddressZero
+  const swapRouterAddress =
+    echainId === config.networks.arbitrum_one_goerli.chainId!
+      ? ARB_GOERLI_SWAP_ROUTER_ADDRESS
+      : SWAP_ROUTER_02_ADDRESSES(echainId)
 
-  let { address: liquidator } = await deploy("USUMLiquidator", {
+  const { address: keeperFeePayer } = await deploy("KeeperFeePayer", {
     from: deployer,
-    args: [automateAddress],
+    args: [swapRouterAddress, WETH9[echainId].address],
   })
+  console.log(chalk.yellow(`✨ KeeperFeePayer: ${keeperFeePayer}`))
 
-  // // FIXME: need ops mock or real one
-  // await deploy("USUMMarketFactory", {
-  //   from: deployer,
-  //   args: [oracleRegistry, liquidator, keeperFeePayer],
-  // });
+  const { address: liquidator } = await deploy("USUMLiquidator", {
+    from: deployer,
+    args: [GELATO_ADDRESSES[echainId].automate],
+  })
   console.log(chalk.yellow(`✨ USUMLiquidator: ${liquidator}`))
+
+  const { address: lpSlotSet } = await deploy("LpSlotSetLib", {
+    from: deployer,
+  })
+  console.log(chalk.yellow(`✨ LpSlotSetLib: ${lpSlotSet}`))
+
+  const { address: marketDeployer } = await deploy("MarketDeployerLib", {
+    from: deployer,
+    libraries: {
+      LpSlotSetLib: lpSlotSet,
+    },
+  })
+  console.log(chalk.yellow(`✨ MarketDeployerLib: ${marketDeployer}`))
+
+  const { address: oracleProviderRegistry } = await deploy(
+    "OracleProviderRegistryLib",
+    {
+      from: deployer,
+    }
+  )
+  console.log(
+    chalk.yellow(`✨ OracleProviderRegistryLib: ${oracleProviderRegistry}`)
+  )
+
+  const { address: settlementTokenRegistry } = await deploy(
+    "SettlementTokenRegistryLib",
+    {
+      from: deployer,
+    }
+  )
+  console.log(
+    chalk.yellow(`✨ SettlementTokenRegistryLib: ${settlementTokenRegistry}`)
+  )
+
+  const { address: factory } = await deploy("USUMMarketFactory", {
+    from: deployer,
+    args: [liquidator, keeperFeePayer],
+    libraries: {
+      MarketDeployerLib: marketDeployer,
+      OracleProviderRegistryLib: oracleProviderRegistry,
+      SettlementTokenRegistryLib: settlementTokenRegistry,
+    },
+  })
+  console.log(chalk.yellow(`✨ USUMMarketFactory: ${factory}`))
 }
 
 export default func
