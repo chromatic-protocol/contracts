aab08e2d76814f826c4005f2c61c00af2ccda160
daniel-fi <daniel.fi@quarkonix.com>
2023-05-17 16:41:54 +0900

    docs: `LpSlotSet` Fixes #129

--- contracts/core/external/lpslot/LpSlotSet.sol
+++ contracts/core/external/lpslot/LpSlotSet.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity >=0.8.0 <0.9.0;
 
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
@@ -21,12 +21,22 @@ struct LpSlotSet {
 
 using LpSlotSetLib for LpSlotSet global;
 
+/**
+ * @title LpSlotSetLib
+ * @notice Library for managing liquidity slots in an LPSlotSet
+ */
 library LpSlotSetLib {
     using Math for uint256;
     using SafeCast for uint256;
     using SignedMath for int256;
     using LpSlotLib for LpSlot;
 
+    /**
+     * @notice Emitted when earning is accumulated for a liquidity slot.
+     * @param feeRate The fee rate of the slot.
+     * @param slotType The type of the slot ("L" for long, "S" for short).
+     * @param earning The accumulated earning.
+     */
     event LpSlotEarningAccumulated(
         uint16 indexed feeRate,
         bytes1 slotType,
@@ -41,12 +51,33 @@ library LpSlotSetLib {
         uint256 takerMargin;
     }
 
+    /**
+     * @notice Modifier to validate the trading fee rate.
+     * @param tradingFeeRate The trading fee rate to validate.
+     */
     modifier _validTradingFeeRate(int16 tradingFeeRate) {
         validateTradingFeeRate(tradingFeeRate);
 
         _;
     }
 
+    /**
+     * @notice Prepares slot margins based on the given quantity and maker margin.
+     * @dev This function prepares slot margins by performing the following steps:
+     *      1. Calculates the appropriate slot margins
+     *         for each trading fee rate based on the provided quantity and maker margin.
+     *      2. Iterates through the target slots based on the quantity,
+     *         finds the minimum available fee rate,
+     *         and determines the upper bound for calculating slot margins.
+     *      3. Iterates from the minimum fee rate until the upper bound,
+     *         assigning the remaining maker margin to the slots until it is exhausted.
+     *      4. Creates an array of LpSlotMargin structs
+     *         containing the trading fee rate and corresponding margin amount for each slot.
+     * @param self The reference to the LpSlotSet.
+     * @param qty The quantity of the position.
+     * @param makerMargin The maker margin of the position.
+     * @return slotMargins An array of LpSlotMargin representing the calculated slot margins.
+     */
     function prepareSlotMargins(
         LpSlotSet storage self,
         int224 qty,
@@ -88,6 +119,25 @@ library LpSlotSetLib {
         return slotMargins;
     }
 
+    /**
+     * @notice Accepts an open position and updates the liquidity slots accordingly.
+     * @dev This function accepts an open position by performing the following steps:
+     *      1. Retrieves the target slots based on the position's quantity.
+     *      2. Obtains the maker margin and slot margins from the position.
+     *      3. Divides the leveraged quantity, maker margin, taker margin,
+     *         and slot margins into proportional position parameter values.
+     *      4. Creates a new PositionParam memory with the oracle version and timestamp from the position.
+     *      5. Iterates through the slot margins and processes the open position
+     *         for each non-zero slot margin amount.
+     *         - Sets the leveraged quantity, taker margin, and maker margin in the PositionParam.
+     *         - Calls the openPosition function on the corresponding slot,
+     *           passing the LpContext, PositionParam, and trading fee from the slot margin.
+     *      6. Sets the minimum available fee rate in the LpSlotSet
+     *         based on the trading fee rate of the last slot margin.
+     * @param self The reference to the LpSlotSet.
+     * @param ctx The LpContext object.
+     * @param position The Position object representing the open position.
+     */
     function acceptOpenPosition(
         LpSlotSet storage self,
         LpContext memory ctx,
@@ -136,6 +186,27 @@ library LpSlotSetLib {
         );
     }
 
+    /**
+     * @notice Accepts a close position request and performs necessary operations.
+     * @dev This function accepts a close position by performing the following steps:
+     *      1. Validates the provided realizedPnl against the position's maker and taker margins.
+     *      2. Calculates the position parameter values for each slot
+     *         based on the leveraged quantity, maker margin, taker margin, and slot margins.
+     *      3. Calls the closePosition function on each slot
+     *         with the calculated parameters to close the position.
+     *         - If the realizedPnl is zero, it closes all slots with non-zero amounts.
+     *         - If the realizedPnl is positive and equal to the maker margin,
+     *           it closes all slots with non-zero amounts and sets the taker profit as the pnl for each slot.
+     *         - Otherwise, it iterates through each slot and calculates the appropriate taker pnl
+     *           based on the remaining realizedPnl and maker margin. This function then closes each slot
+     *           with the calculated taker pnl.
+     *      4. Updates the minimum available fee rate based on the slotMargins.
+     * @dev This function throws an error if the realizedPnl exceeds the margin range.
+     * @param self The storage reference to the LpSlotSet.
+     * @param ctx The LpContext structure containing contextual information.
+     * @param position The Position structure representing the position to be closed.
+     * @param realizedPnl The realized position profit/loss (taker side).
+     */
     function acceptClosePosition(
         LpSlotSet storage self,
         LpContext memory ctx,
@@ -247,6 +318,20 @@ library LpSlotSetLib {
         }
     }
 
+    /**
+     * @notice Adds liquidity to the liquidity pool.
+     * @dev This function adds liquidity to the liquidity pool by performing the following steps:
+     *      1. Retrieves the target slot based on the trading fee rate.
+     *      2. Calls the addLiquidity function on the target slot,
+     *         passing the LpContext, amount, and totalLiquidity.
+     *      3. Updates the minimum available fee rate if the trading fee rate is lower than the current minimum.
+     * @param self The storage reference to the LpSlotSet.
+     * @param ctx The LpContext memory containing the context information for the liquidity operation.
+     * @param tradingFeeRate The trading fee rate associated with the liquidity being added.
+     * @param amount The amount of liquidity being added.
+     * @param totalLiquidity The total supplied LP token.
+     * @return liquidity The amount of LP token to be minted.
+     */
     function addLiquidity(
         LpSlotSet storage self,
         LpContext memory ctx,
@@ -268,6 +353,20 @@ library LpSlotSetLib {
         }
     }
 
+    /**
+     * @notice Removes liquidity from the liquidity pool.
+     * @dev This function removes liquidity from the liquidity pool by performing the following steps:
+     *      1. Retrieves the target slot based on the trading fee rate.
+     *      2. Calls the removeLiquidity function on the target slot,
+     *         passing the LpContext, liquidity, and totalLiquidity.
+     *      3. Returns the amount of liquidity that was removed.
+     * @param self The storage reference to the LpSlotSet.
+     * @param ctx The LpContext memory containing the context information for the liquidity operation.
+     * @param tradingFeeRate The trading fee rate associated with the liquidity being removed.
+     * @param liquidity The amount of LP token to be burned.
+     * @param totalLiquidity The total supplied LP token.
+     * @return amount The amount of liquidity that was removed.
+     */
     function removeLiquidity(
         LpSlotSet storage self,
         LpContext memory ctx,
@@ -280,6 +379,14 @@ library LpSlotSetLib {
         amount = slot.removeLiquidity(ctx, liquidity, totalLiquidity);
     }
 
+    /**
+     * @notice Retrieves the total slot margin amount for a given trading fee rate.
+     * @dev This function retrieves the total slot margin amount for the specified trading fee rate
+     *      by retrieving the target slot and returning its total balance.
+     * @param self The storage reference to the LpSlotSet.
+     * @param tradingFeeRate The trading fee rate associated with the slot.
+     * @return amount The total slot margin amount for the given trading fee rate.
+     */
     function getSlotMarginTotal(
         LpSlotSet storage self,
         int16 tradingFeeRate
@@ -288,6 +395,14 @@ library LpSlotSetLib {
         return slot.total;
     }
 
+    /**
+     * @notice Retrieves the unused slot margin amount for a given trading fee rate.
+     * @dev This function retrieves the unused slot margin amount for the specified trading fee rate
+     *      by retrieving the target slot and returning its balance.
+     * @param self The storage reference to the LpSlotSet.
+     * @param tradingFeeRate The trading fee rate associated with the slot.
+     * @return amount The unused slot margin amount for the given trading fee rate.
+     */
     function getSlotMarginUnused(
         LpSlotSet storage self,
         int16 tradingFeeRate
@@ -296,6 +411,13 @@ library LpSlotSetLib {
         return slot.balance();
     }
 
+    /**
+     * @notice Retrieves the target slots based on the sign of the given value.
+     * @dev This function retrieves the target slots mapping (short or long) based on the sign of the given value.
+     * @param self The storage reference to the LpSlotSet.
+     * @param sign The sign of the value (-1 for negative, 1 for positive).
+     * @return _slots The target slots mapping associated with the sign of the value.
+     */
     function targetSlots(
         LpSlotSet storage self,
         int256 sign
@@ -303,6 +425,13 @@ library LpSlotSetLib {
         return sign < 0 ? self._shortSlots : self._longSlots;
     }
 
+    /**
+     * @notice Retrieves the target slot based on the trading fee rate.
+     * @dev This function retrieves the target slot based on the sign of the trading fee rate and returns it.
+     * @param self The storage reference to the LpSlotSet.
+     * @param tradingFeeRate The trading fee rate associated with the slot.
+     * @return slot The target slot associated with the trading fee rate.
+     */
     function targetSlot(
         LpSlotSet storage self,
         int16 tradingFeeRate
@@ -313,6 +442,14 @@ library LpSlotSetLib {
                 : self._longSlots[abs(tradingFeeRate)];
     }
 
+    /**
+     * @notice Retrieves the minimum available fee rate based on the sign of the given value.
+     * @dev This function retrieves the minimum available fee rate based on the sign of the given value.
+     *      If the fee rate is zero, it returns the minimum fee rate defined as MIN_FEE_RATE.
+     * @param self The storage reference to the LpSlotSet.
+     * @param sign The sign of the value (-1 for negative, 1 for positive).
+     * @return feeRate The minimum available fee rate associated with the sign of the value.
+     */
     function minAvailableFeeRate(
         LpSlotSet storage self,
         int256 sign
@@ -323,6 +460,16 @@ library LpSlotSetLib {
         return feeRate == 0 ? MIN_FEE_RATE : feeRate;
     }
 
+    /**
+     * @notice Sets the minimum available fee rate based on the position and fee rate.
+     * @dev This function sets the minimum available fee rate based on the position and fee rate.
+     *      If the position quantity is negative, it sets the fee rate
+     *      as the minimum available fee rate for short positions.
+     *      Otherwise, it sets the fee rate as the minimum available fee rate for long positions.
+     * @param self The storage reference to the LpSlotSet.
+     * @param position The position information.
+     * @param feeRate The fee rate to be set as the minimum available fee rate.
+     */
     function setMinAvailableFeeRate(
         LpSlotSet storage self,
         Position memory position,
@@ -335,6 +482,15 @@ library LpSlotSetLib {
         }
     }
 
+    /**
+     * @notice Sets the minimum available fee rate based on the fee rate.
+     * @dev This function sets the minimum available fee rate based on the fee rate.
+     *      If the fee rate is negative, it sets the absolute value of the fee rate
+     *      as the minimum available fee rate for short positions.
+     *      Otherwise, it sets the fee rate as the minimum available fee rate for long positions.
+     * @param self The storage reference to the LpSlotSet.
+     * @param feeRate The fee rate to be set as the minimum available fee rate.
+     */
     function setMinAvailableFeeRate(
         LpSlotSet storage self,
         int16 feeRate
@@ -346,6 +502,18 @@ library LpSlotSetLib {
         }
     }
 
+    /**
+     * @notice Divides the leveraged quantity, maker margin, and taker margin
+     *         into proportional position parameter values.
+     * @dev This function divides the leveraged quantity, maker margin, and taker margin
+     *      into proportional position parameter values based on the slot margins.
+     *      It calculates the proportional values for each slot margin and returns them in an array.
+     * @param leveragedQty The leveraged quantity.
+     * @param makerMargin The maker margin amount.
+     * @param takerMargin The taker margin amount.
+     * @param slotMargins The array of slot margins.
+     * @return values The array of proportional position parameter values.
+     */
     function divideToPositionParamValue(
         int256 leveragedQty,
         uint256 makerMargin,
@@ -391,6 +559,12 @@ library LpSlotSetLib {
         return values;
     }
 
+    /**
+     * @notice Creates a new PositionParam struct with the given oracle version and timestamp.
+     * @param oracleVersion The version of the oracle.
+     * @param timestamp The timestamp.
+     * @return param The new PositionParam struct.
+     */
     function newPositionParam(
         uint256 oracleVersion,
         uint256 timestamp
@@ -399,6 +573,13 @@ library LpSlotSetLib {
         param.timestamp = timestamp;
     }
 
+    /**
+     * @notice Validates the trading fee rate.
+     * @dev This function validates the trading fee rate by checking if it is supported.
+     *      It compares the absolute value of the fee rate with the predefined trading fee rates
+     *      to determine if it is a valid rate.
+     * @param tradingFeeRate The trading fee rate to be validated.
+     */
     function validateTradingFeeRate(int16 tradingFeeRate) private pure {
         uint16[FEE_RATES_LENGTH] memory _tradingFeeRates = tradingFeeRates();
 
@@ -412,10 +593,21 @@ library LpSlotSetLib {
         );
     }
 
+    /**
+     * @notice Calculates the absolute value of an int16 number.
+     * @param i The int16 number.
+     * @return absValue The absolute value of the input number.
+     */
     function abs(int16 i) private pure returns (uint16) {
         return i < 0 ? uint16(-i) : uint16(i);
     }
 
+    /**
+     * @notice Retrieves the array of supported trading fee rates.
+     * @dev This function returns the array of supported trading fee rates,
+     *      ranging from the minimum fee rate to the maximum fee rate with step increments.
+     * @return tradingFeeRates The array of supported trading fee rates.
+     */
     function tradingFeeRates()
         private
         pure
@@ -425,11 +617,21 @@ library LpSlotSetLib {
         return [
             MIN_FEE_RATE, 2, 3, 4, 5, 6, 7, 8, 9, // 0.01% ~ 0.09%, step 0.01%
             10, 20, 30, 40, 50, 60, 70, 80, 90, // 0.1% ~ 0.9%, step 0.1%
-            100, 200, 300, 400, 500, 600, 700, 700, 900, // 1% ~ 9%, step 1%
+            100, 200, 300, 400, 500, 600, 700, 800, 900, // 1% ~ 9%, step 1%
             1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000 // 10% ~ 50%, step 5%
         ];
     }
 
+    /**
+     * @notice Finds the upper bound index of an element in a sorted array.
+     * @dev This function performs a binary search on the sorted array
+     *      to find * the index of the upper bound of the given element.
+     *      It returns the index as the exclusive upper bound,
+     *      or the inclusive upper bound if the element is found at the end of the array.
+     * @param array The sorted array.
+     * @param element The element to find the upper bound for.
+     * @return uint256 The index of the upper bound of the element in the array.
+     */
     function findUpperBound(
         uint16[FEE_RATES_LENGTH] memory array,
         uint16 element
@@ -461,6 +663,17 @@ library LpSlotSetLib {
         }
     }
 
+    /**
+     * @notice Distributes earnings among the liquidity slots.
+     * @dev This function distributes the earnings among the liquidity slots,
+     *      proportional to their total balances.
+     *      It iterates through the trading fee rates
+     *      and distributes the proportional amount of earnings to each slot
+     *      based on its total balance relative to the market balance.
+     * @param self The LpSlotSet storage.
+     * @param earning The total earnings to be distributed.
+     * @param marketBalance The market balance.
+     */
     function distributeEarning(
         LpSlotSet storage self,
         uint256 earning,
@@ -486,6 +699,21 @@ library LpSlotSetLib {
         );
     }
 
+    /**
+     * @notice Distributes earnings among the liquidity slots of a specific type.
+     * @dev This function distributes the earnings among the liquidity slots of
+     *      the specified type, proportional to their total balances.
+     *      It iterates through the trading fee rates
+     *      and distributes the proportional amount of earnings to each slot
+     *      based on its total balance relative to the market balance.
+     * @param lpSlots The liquidity slots mapping.
+     * @param earning The total earnings to be distributed.
+     * @param marketBalance The market balance.
+     * @param _tradingFeeRates The array of supported trading fee rates.
+     * @param slotType The type of liquidity slot ("L" for long, "S" for short).
+     * @return remainEarning The remaining earnings after distribution.
+     * @return remainBalance The remaining market balance after distribution.
+     */
     function distributeEarning(
         mapping(uint16 => LpSlot) storage lpSlots,
         uint256 earning,
