5dd14c0dc2a75158a7d9a389c72577a3cc1daf0d
daniel-fi <daniel.fi@quarkonix.com>
2023-05-17 14:04:55 +0900

    docs: `MarketDeployer` Fixes #129

--- contracts/core/external/deployer/MarketDeployer.sol
+++ contracts/core/external/deployer/MarketDeployer.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity >=0.8.0 <0.9.0;
 
 import {USUMMarket} from "@usum/core/USUMMarket.sol";
@@ -12,8 +12,18 @@ struct Parameters {
     address settlementToken;
 }
 
+/**
+ * @title MarketDeployer
+ * @notice This library provides a function to deploy a USUMMarket contract.
+ */
 library MarketDeployerLib {
-
+    /**
+     * @notice Deploys a USUMMarket contract.
+     * @param self The MarketDeployer storage.
+     * @param oracleProvider The address of the oracle provider.
+     * @param settlementToken The address of the settlement token.
+     * @return market The address of the deployed USUMMarket contract.
+     */
     function deploy(
         MarketDeployer storage self,
         address oracleProvider,
