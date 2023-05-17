d8e34ca1d5a0d5a485773ca495515a4964fdd2f0
mark-onix <mark.kk@quarkonix.com>
2023-05-17 22:18:19 +0900

    remove lens

--- test/hardhat/periphery/lens/deploy_lens.ts
+++
@@ -1,8 +0,0 @@
-import { deployContract, hardhatErrorPrettyPrint } from "../../utils"
-import { USUMLens } from "@usum/typechain-types"
-
-export async function deploy() {
-  return hardhatErrorPrettyPrint(async () => {
-   return await deployContract<USUMLens>('USUMLens')
-  })
-}
