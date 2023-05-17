e60d387374bd080a8856609ec1d6da6664e72602
mark-onix <mark.kk@quarkonix.com>
2023-05-17 22:18:58 +0900

    fix test USUMVault deploy arg

--- test/hardhat/market_factory/deploy.ts
+++ test/hardhat/market_factory/deploy.ts
@@ -3,22 +3,22 @@ import {
   USUMLiquidator,
   USUMMarketFactory,
   USUMVault,
-} from "@usum/typechain-types"
-import { Contract } from "ethers"
-import { ethers } from "hardhat"
-import { deployContract } from "../utils"
+} from "@usum/typechain-types";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+import { deployContract } from "../utils";
 
 export async function deploy(opsAddress: string, opsProxyFactory: string) {
-  const [deployer] = await ethers.getSigners()
+  const [deployer] = await ethers.getSigners();
 
   const oracleProviderRegistryLib = await deployContract<Contract>(
     "OracleProviderRegistryLib"
-  )
+  );
   const settlementTokenRegistryLib = await deployContract<Contract>(
     "SettlementTokenRegistryLib"
-  )
+  );
 
-  const lpSlotSetLib = await deployContract<Contract>("LpSlotSetLib")
+  const lpSlotSetLib = await deployContract<Contract>("LpSlotSetLib");
   const marketDeployerLib = await deployContract<Contract>(
     "MarketDeployerLib",
     {
@@ -26,7 +26,7 @@ export async function deploy(opsAddress: string, opsProxyFactory: string) {
         LpSlotSetLib: lpSlotSetLib.address,
       },
     }
-  )
+  );
 
   const marketFactory = await deployContract<USUMMarketFactory>(
     "USUMMarketFactory",
@@ -37,29 +37,29 @@ export async function deploy(opsAddress: string, opsProxyFactory: string) {
         MarketDeployerLib: marketDeployerLib.address,
       },
     }
-  )
+  );
 
   const keeperFeePayer = await deployContract<KeeperFeePayerMock>(
     "KeeperFeePayerMock",
     { args: [marketFactory.address] }
-  )
-  await (await marketFactory.setKeeperFeePayer(keeperFeePayer.address)).wait()
+  );
+  await (await marketFactory.setKeeperFeePayer(keeperFeePayer.address)).wait();
   await (
     await deployer.sendTransaction({
       to: keeperFeePayer.address,
       value: ethers.utils.parseEther("5"),
     })
-  ).wait()
+  ).wait();
 
   const vault = await deployContract<USUMVault>("USUMVault", {
-    args: [marketFactory.address],
-  })
-  await (await marketFactory.setVault(vault.address)).wait()
+    args: [marketFactory.address, opsAddress, opsProxyFactory],
+  });
+  await (await marketFactory.setVault(vault.address)).wait();
 
   const liquidator = await deployContract<USUMLiquidator>("USUMLiquidator", {
     args: [marketFactory.address, opsAddress, opsProxyFactory],
-  })
-  await (await marketFactory.setLiquidator(liquidator.address)).wait()
+  });
+  await (await marketFactory.setLiquidator(liquidator.address)).wait();
 
-  return { marketFactory, keeperFeePayer, liquidator }
+  return { marketFactory, keeperFeePayer, liquidator };
 }
