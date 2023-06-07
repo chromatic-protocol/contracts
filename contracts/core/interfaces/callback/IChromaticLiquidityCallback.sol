b114e6939c033193b530729947f1e8ed036c4465
daniel-fi <daniel.fi@quarkonix.com>
2023-06-07 14:54:17 +0900

    docs: `callback` Fixes #217

--- contracts/core/interfaces/callback/IChromaticLiquidityCallback.sol
+++ contracts/core/interfaces/callback/IChromaticLiquidityCallback.sol
@@ -1,20 +1,48 @@
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
+/**
+ * @title IChromaticLiquidityCallback
+ * @dev Interface for a contract that handles liquidity callbacks in the Chromatic protocol.
+ *      Liquidity callbacks are used to handle various operations related to liquidity management.
+ *      This interface defines the function signatures for different types of liquidity callbacks.
+ */
 interface IChromaticLiquidityCallback {
+    /**
+     * @notice Handles the callback after adding liquidity to the Chromatic protocol.
+     * @param settlementToken The address of the settlement token used for adding liquidity.
+     * @param vault The address of the vault where the liquidity is added.
+     * @param data Additional data associated with the liquidity addition.
+     */
     function addLiquidityCallback(
         address settlementToken,
         address vault,
         bytes calldata data
     ) external;
 
+    /**
+     * @notice Handles the callback after claiming liquidity from the Chromatic protocol.
+     * @param receiptId The ID of the liquidity claim receipt.
+     * @param data Additional data associated with the liquidity claim.
+     */
     function claimLiquidityCallback(uint256 receiptId, bytes calldata data) external;
 
+    /**
+     * @notice Handles the callback after removing liquidity from the Chromatic protocol.
+     * @param clbToken The address of the Chromatic liquidity token.
+     * @param clbTokenId The ID of the Chromatic liquidity token to be removed.
+     * @param data Additional data associated with the liquidity removal.
+     */
     function removeLiquidityCallback(
         address clbToken,
         uint256 clbTokenId,
         bytes calldata data
     ) external;
 
+    /**
+     * @notice Handles the callback after withdrawing liquidity from the Chromatic protocol.
+     * @param receiptId The ID of the liquidity withdrawal receipt.
+     * @param data Additional data associated with the liquidity withdrawal.
+     */
     function withdrawLiquidityCallback(uint256 receiptId, bytes calldata data) external;
 }
