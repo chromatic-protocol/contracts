31f4ff1743878a64e0c224ba52074acacd2a6a6e
daniel-fi <daniel.fi@quarkonix.com>
2023-06-07 16:16:55 +0900

    docs: `factory` Fixes #225

--- contracts/core/interfaces/IChromaticMarketFactory.sol
+++ contracts/core/interfaces/IChromaticMarketFactory.sol
@@ -6,64 +6,179 @@ import {IMarketDeployer} from "@chromatic/core/interfaces/factory/IMarketDeploye
 import {ISettlementTokenRegistry} from "@chromatic/core/interfaces/factory/ISettlementTokenRegistry.sol";
 import {IOracleProviderRegistry} from "@chromatic/core/interfaces/factory/IOracleProviderRegistry.sol";
 
+/**
+ * @title IChromaticMarketFactory
+ * @dev Interface for the Chromatic Market Factory contract.
+ */
 interface IChromaticMarketFactory is
     IMarketDeployer,
     IOracleProviderRegistry,
     ISettlementTokenRegistry,
     IInterestCalculator
 {
+    /**
+     * @notice Emitted when the DAO address is updated.
+     * @param dao The new DAO address.
+     */
     event UpdateDao(address indexed dao);
+
+    /**
+     * @notice Emitted when the DAO treasury address is updated.
+     * @param treasury The new DAO treasury address.
+     */
     event UpdateTreasury(address indexed treasury);
+
+    /**
+     * @notice Emitted when the liquidator address is set.
+     * @param liquidator The liquidator address.
+     */
     event SetLiquidator(address indexed liquidator);
+
+    /**
+     * @notice Emitted when the vault address is set.
+     * @param vault The vault address.
+     */
     event SetVault(address indexed vault);
+
+    /**
+     * @notice Emitted when the keeper fee payer address is set.
+     * @param keeperFeePayer The keeper fee payer address.
+     */
     event SetKeeperFeePayer(address indexed keeperFeePayer);
 
+    /**
+     * @notice Emitted when a market is created.
+     * @param oracleProvider The address of the oracle provider.
+     * @param settlementToken The address of the settlement token.
+     * @param market The address of the created market.
+     */
     event MarketCreated(
         address indexed oracleProvider,
         address indexed settlementToken,
         address indexed market
     );
 
+    /**
+     * @notice Returns the address of the DAO.
+     * @return The address of the DAO.
+     */
     function dao() external view returns (address);
 
+    /**
+     * @notice Returns the address of the DAO treasury.
+     * @return The address of the DAO treasury.
+     */
     function treasury() external view returns (address);
 
+    /**
+     * @notice Returns the address of the liquidator.
+     * @return The address of the liquidator.
+     */
     function liquidator() external view returns (address);
 
+    /**
+     * @notice Returns the address of the vault.
+     * @return The address of the vault.
+     */
     function vault() external view returns (address);
 
+    /**
+     * @notice Returns the address of the keeper fee payer.
+     * @return The address of the keeper fee payer.
+     */
     function keeperFeePayer() external view returns (address);
 
+    /**
+     * @notice Updates the DAO address.
+     * @param dao The new DAO address.
+     */
     function updateDao(address dao) external;
 
+    /**
+     * @notice Updates the DAO treasury address.
+     * @param treasury The new DAO treasury address.
+     */
     function updateTreasury(address treasury) external;
 
+    /**
+     * @notice Sets the liquidator address.
+     * @param liquidator The liquidator address.
+     */
     function setLiquidator(address liquidator) external;
 
+    /**
+     * @notice Sets the vault address.
+     * @param vault The vault address.
+     */
     function setVault(address vault) external;
 
+    /**
+     * @notice Sets the keeper fee payer address.
+     * @param keeperFeePayer The keeper fee payer address.
+     */
     function setKeeperFeePayer(address keeperFeePayer) external;
 
-    function getMarkets() external view returns (address[] memory market);
+    /**
+     * @notice Returns an array of all market addresses.
+     * @return markets An array of all market addresses.
+     */
+    function getMarkets() external view returns (address[] memory markets);
 
+    /**
+     * @notice Returns an array of market addresses associated with a settlement token.
+     * @param settlementToken The address of the settlement token.
+     * @return An array of market addresses.
+     */
     function getMarketsBySettlmentToken(
         address settlementToken
     ) external view returns (address[] memory);
 
+    /**
+     * @notice Returns the address of a market associated with an oracle provider and settlement token.
+     * @param oracleProvider The address of the oracle provider.
+     * @param settlementToken The address of the settlement token.
+     * @return The address of the market.
+     */
     function getMarket(
         address oracleProvider,
         address settlementToken
     ) external view returns (address);
 
+    /**
+     * @notice Creates a new market associated with an oracle provider and settlement token.
+     * @param oracleProvider The address of the oracle provider.
+     * @param settlementToken The address of the settlement token.
+     */
     function createMarket(address oracleProvider, address settlementToken) external;
 
+    /**
+     * @notice Checks if a market is registered.
+     * @param market The address of the market.
+     * @return True if the market is registered, false otherwise.
+     */
     function isRegisteredMarket(address market) external view returns (bool);
 
+    /**
+     * @notice Creates a Maker earning distribution task for a token.
+     * @param token The address of the token.
+     */
     function createMakerEarningDistributionTask(address token) external;
 
+    /**
+     * @notice Cancels a Maker earning distribution task for a token.
+     * @param token The address of the token.
+     */
     function cancelMakerEarningDistributionTask(address token) external;
 
+    /**
+     * @notice Creates a market earning distribution task for a market.
+     * @param market The address of the market.
+     */
     function createMarketEarningDistributionTask(address market) external;
 
+    /**
+     * @notice Cancels a market earning distribution task for a market.
+     * @param market The address of the market.
+     */
     function cancelMarketEarningDistributionTask(address market) external;
 }
--- contracts/core/interfaces/factory/IMarketDeployer.sol
+++ contracts/core/interfaces/factory/IMarketDeployer.sol
@@ -1,16 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-/// @title An interface for a contract that is capable of deploying Chromatic markets
-/// @notice A contract that constructs a market must implement this to pass arguments to the market
-/// @dev This is used to avoid having constructor arguments in the market contract, which results in the init code hash
-/// of the market being constant allowing the CREATE2 address of the market to be cheaply computed on-chain
+/**
+ * @title An interface for a contract that is capable of deploying Chromatic markets
+ * @notice A contract that constructs a market must implement this to pass arguments to the market
+ * @dev This is used to avoid having constructor arguments in the market contract, which results in the init code hash
+ * of the market being constant allowing the CREATE2 address of the market to be cheaply computed on-chain
+ */
 interface IMarketDeployer {
-    /// @notice Get the parameters to be used in constructing the market, set transiently during market creation.
-    /// @dev Called by the market constructor to fetch the parameters of the market
-    /// Returns underlyingAsset The underlying asset of the market
-    /// Returns settlementToken The settlement token of the market
-    /// Returns vPoolCapacity Capacity of virtual future pool
-    /// Returns vPoolA Amplification coefficient of virtual future pool, precise value
+    /**
+     * @notice Get the parameters to be used in constructing the market, set transiently during market creation.
+     * @dev Called by the market constructor to fetch the parameters of the market
+     * Returns underlyingAsset The underlying asset of the market
+     * Returns settlementToken The settlement token of the market
+     * Returns vPoolCapacity Capacity of virtual future pool
+     * Returns vPoolA Amplification coefficient of virtual future pool, precise value
+     */
     function parameters() external view returns (address oracleProvider, address settlementToken);
 }
