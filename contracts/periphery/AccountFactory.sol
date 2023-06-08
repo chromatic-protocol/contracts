93d46a49eb4af0f6ef0ee19089c13e148d0c1bb4
daniel-fi <daniel.fi@quarkonix.com>
2023-06-08 16:25:23 +0900

    docs: `AccountFactory` Fixes #249

--- contracts/periphery/AccountFactory.sol
+++ contracts/periphery/AccountFactory.sol
@@ -6,35 +6,61 @@ import {IAccount} from "./interfaces/IAccount.sol";
 import {IAccountFactory} from "./interfaces/IAccountFactory.sol";
 import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
 
+/**
+ * @title AccountFactory
+ * @dev Contract for creating and managing user accounts.
+ */
 contract AccountFactory is IAccountFactory {
     Account private cloneBase;
     address private router;
     address private marketFactory;
     mapping(address => address) private accounts;
 
+    /**
+     * @dev Initializes the AccountFactory contract with the provided router and market factory addresses.
+     * @param _router The address of the router contract.
+     * @param _marketFactory The address of the market factory contract.
+     */
     constructor(address _router, address _marketFactory) {
         cloneBase = new Account();
         router = _router;
         marketFactory = _marketFactory;
     }
 
+    /**
+     * @dev Modifier to allow only the router contract to call a function.
+     */
     modifier onlyRouter() {
         require(msg.sender == router, "Only Router can call");
         _;
     }
 
+    /**
+     * @inheritdoc IAccountFactory
+     * @dev Only one account can be created per user.
+     *      Emits an `AccountCreated` event upon successful creation.
+     */
     function createAccount() external {
-        require(accounts[msg.sender] == address(0));
+        address owner = msg.sender;
+        require(accounts[owner] == address(0));
+
         Account newAccount = Account(Clones.clone(address(cloneBase)));
-        newAccount.initialize(msg.sender, router, marketFactory);
-        accounts[msg.sender] = address(newAccount);
-        emit AccountCreated(address(newAccount));
+        newAccount.initialize(owner, router, marketFactory);
+        accounts[owner] = address(newAccount);
+
+        emit AccountCreated(address(newAccount), owner);
     }
 
+    /**
+     * @inheritdoc IAccountFactory
+     */
     function getAccount(address accountAddress) external view onlyRouter returns (address) {
         return accounts[accountAddress];
     }
 
+    /**
+     * @inheritdoc IAccountFactory
+     */
     function getAccount() external view returns (address) {
         return accounts[msg.sender];
     }
--- contracts/periphery/interfaces/IAccountFactory.sol
+++ contracts/periphery/interfaces/IAccountFactory.sol
@@ -1,12 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
+/**
+ * @title IAccountFactory
+ * @dev Interface for the AccountFactory contract, which creates and manages user accounts.
+ */
 interface IAccountFactory {
-    event AccountCreated(address account);
+    /**
+     * @dev Emitted when a new account is created.
+     * @param account The address of the created account.
+     * @param owner The address of the owner of the created account.
+     */
+    event AccountCreated(address indexed account, address indexed owner);
 
+    /**
+     * @notice Creates a new user account.
+     */
     function createAccount() external;
 
+    /**
+     * @notice Retrieves the address of a user's account.
+     * @param accountAddress The address of the user's account.
+     * @return The address of the user's account.
+     */
     function getAccount(address accountAddress) external view returns (address);
 
+    /**
+     * @notice Retrieves the address of the caller's account.
+     * @return The address of the caller's account.
+     */
     function getAccount() external view returns (address);
 }
