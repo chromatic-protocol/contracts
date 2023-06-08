17865948e99e135fa65cdbd3ddc693ad168542cb
daniel-fi <daniel.fi@quarkonix.com>
2023-06-08 15:05:12 +0900

    docs: `KeeperFeePayer` Fixes #243

--- contracts/core/KeeperFeePayer.sol
+++ contracts/core/KeeperFeePayer.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity >=0.8.0 <0.9.0;
 
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -8,6 +8,10 @@ import {IKeeperFeePayer} from "@chromatic/core/interfaces/IKeeperFeePayer.sol";
 import {IChromaticMarketFactory} from "@chromatic/core/interfaces/IChromaticMarketFactory.sol";
 import {Errors} from "@chromatic/core/libraries/Errors.sol";
 
+/**
+ * @title KeeperFeePayer
+ * @dev A contract that pays keeper fees using a Uniswap router.
+ */
 contract KeeperFeePayer is IKeeperFeePayer {
     IChromaticMarketFactory factory;
     ISwapRouter uniswapRouter;
@@ -15,32 +19,56 @@ contract KeeperFeePayer is IKeeperFeePayer {
 
     event SetRouter(address indexed);
 
+    /**
+     * @dev Modifier to restrict access to only the DAO.
+     */
     modifier onlyDao() {
         require(msg.sender == factory.dao(), Errors.ONLY_DAO_CAN_ACCESS);
         _;
     }
 
+    /**
+     * @dev Modifier to restrict access to only the Vault.
+     */
     modifier onlyVault() {
         require(msg.sender == factory.vault(), Errors.ONLY_VAULT_CAN_ACCESS);
         _;
     }
 
+    /**
+     * @dev Constructor function.
+     * @param _factory The address of the ChromaticMarketFactory contract.
+     * @param _uniswapRouter The address of the Uniswap router contract.
+     * @param _weth The address of the WETH9 contract.
+     */
     constructor(IChromaticMarketFactory _factory, ISwapRouter _uniswapRouter, IWETH9 _weth) {
         factory = _factory;
         uniswapRouter = _uniswapRouter;
         WETH9 = _weth;
     }
 
+    /**
+     * @dev Sets the Uniswap router address.
+     * @param _uniswapRouter The address of the Uniswap router contract.
+     * @notice Only the DAO can call this function.
+     */
     function setRouter(ISwapRouter _uniswapRouter) public onlyDao {
         uniswapRouter = _uniswapRouter;
         emit SetRouter(address(uniswapRouter));
     }
 
-    // this contrct doesn't have balance
+    /**
+     * @inheritdoc IKeeperFeePayer
+     * @dev Only the DAO can call this function.
+     */
     function approveToRouter(address token, bool approve) external onlyDao {
         IERC20(token).approve(address(uniswapRouter), approve ? type(uint256).max : 0);
     }
 
+    /**
+     * @inheritdoc IKeeperFeePayer
+     * @dev Only the Vault can call this function.
+     */
     function payKeeperFee(
         address tokenIn,
         uint256 amountOut,
@@ -62,7 +90,14 @@ contract KeeperFeePayer is IKeeperFeePayer {
         SafeERC20.safeTransfer(IERC20(tokenIn), msg.sender, remainedBalance);
     }
 
-    // real swap execution
+    /**
+     * @dev Executes a Uniswap swap with exact output amount.
+     * @param tokenIn The address of the input token.
+     * @param recipient The address that will receive the output tokens.
+     * @param amountOut The desired amount of output tokens.
+     * @param amountInMaximum The maximum amount of input tokens allowed for the swap.
+     * @return amountIn The actual amount of input tokens used for the swap.
+     */
     function swapExactOutput(
         address tokenIn,
         address recipient,
@@ -82,7 +117,13 @@ contract KeeperFeePayer is IKeeperFeePayer {
         return uniswapRouter.exactOutputSingle(swapParam);
     }
 
+    /**
+     * @dev Fallback function to receive ETH payments.
+     */
     receive() external payable {}
 
+    /**
+     * @dev Fallback function to receive ETH payments.
+     */
     fallback() external payable {}
 }
--- contracts/core/interfaces/IKeeperFeePayer.sol
+++ contracts/core/interfaces/IKeeperFeePayer.sol
@@ -1,9 +1,25 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
+/**
+ * @title IKeeperFeePayer
+ * @dev Interface for a contract that pays keeper fees.
+ */
 interface IKeeperFeePayer {
+    /**
+     * @notice Approves or revokes approval to the Uniswap router for a given token.
+     * @param token The address of the token.
+     * @param approve A boolean indicating whether to approve or revoke approval.
+     */
     function approveToRouter(address token, bool approve) external;
 
+    /**
+     * @notice Pays the keeper fee using Uniswap swaps.
+     * @param tokenIn The address of the token being swapped.
+     * @param amountOut The desired amount of output tokens.
+     * @param keeperAddress The address of the keeper to receive the fee.
+     * @return amountIn The actual amount of input tokens used for the swap.
+     */
     function payKeeperFee(
         address tokenIn,
         uint256 amountOut,
