d8fa437703d729640575afcb3f30ac1b8e12b9ee
not-thing <dade.dd@quarkonix.com>
2023-07-12 16:13:34 +0900

    doc: add errors from `libraries.Errors`

--- docs/errors-templates/helpers.ts
+++ docs/errors-templates/helpers.ts
@@ -1,7 +1,22 @@
-export function isNodeType(item: any, nodeTypeName: string): boolean {
+import { VariableDeclaration } from 'solidity-ast'
+import type { DocItemWithContext } from 'solidity-docgen/src/site'
+
+export function isNodeType(item: DocItemWithContext, nodeTypeName: string): boolean {
   return item.nodeType == nodeTypeName
 }
 
-export function hasErrors(item: any) {
-  return item.errors?.length > 0
+export function isLibErrors(name: string): boolean {
+  return name == 'Errors'
+}
+
+export function hasErrors(item: DocItemWithContext & { errors: any }) {
+  return item.errors?.length > 0 || isLibErrors(item.name)
+}
+
+export function formatVariable(v: VariableDeclaration): string {
+  return [v.typeName?.typeDescriptions.typeString!].concat(v.name || []).join(' ')
+}
+
+export function getConstantValue(v: VariableDeclaration): string {
+  return (v as any).value?.value
 }
