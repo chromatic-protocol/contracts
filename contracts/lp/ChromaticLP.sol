839fe5fb72eb0fe95d48daa8b7d0197f3a4d9ac6
not-thing <dade.dd@quarkonix.com>
2023-09-12 14:18:14 +0900

    chore: clean up code

--- contracts/lp/ChromaticLP.sol
+++ contracts/lp/ChromaticLP.sol
@@ -4,24 +4,17 @@ pragma solidity >=0.8.0 <0.9.0;
 import {Proxy} from "@openzeppelin/contracts/proxy/Proxy.sol";
 import {IERC165} from "@openzeppelin/contracts/interfaces/IERC165.sol";
 import {IERC1155Receiver} from "@openzeppelin/contracts/interfaces/IERC1155Receiver.sol";
-import {IERC1155} from "@openzeppelin/contracts/interfaces/IERC1155.sol";
-import {IOracleProvider} from "@chromatic-protocol/contracts/oracle/interfaces/IOracleProvider.sol";
-
 import {IChromaticLP} from "@chromatic-protocol/contracts/lp/interfaces/IChromaticLP.sol";
 import {ChromaticLPBase} from "@chromatic-protocol/contracts/lp/ChromaticLPBase.sol";
 import {ChromaticLPLogic} from "@chromatic-protocol/contracts/lp/ChromaticLPLogic.sol";
-import {ChromaticLPReceipt, ChromaticLPAction} from "@chromatic-protocol/contracts/lp/libraries/ChromaticLPReceipt.sol";
-import {CLBTokenLib} from "@chromatic-protocol/contracts/core/libraries/CLBTokenLib.sol";
-import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-
+import {ChromaticLPReceipt} from "@chromatic-protocol/contracts/lp/libraries/ChromaticLPReceipt.sol";
 import {IChromaticLiquidityCallback} from "@chromatic-protocol/contracts/core/interfaces/callback/IChromaticLiquidityCallback.sol";
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
 uint16 constant BPS = 10000;
 
 contract ChromaticLP is ChromaticLPBase, Proxy, IChromaticLP, IChromaticLiquidityCallback {
-    using Math for uint256;
+    // using Math for uint256;
     using EnumerableSet for EnumerableSet.UintSet;
 
     address public immutable CHROMATIC_LP_LOGIC;
@@ -56,31 +49,45 @@ contract ChromaticLP is ChromaticLPBase, Proxy, IChromaticLP, IChromaticLiquidit
         return CHROMATIC_LP_LOGIC;
     }
 
+    /**
+     * @inheritdoc IChromaticLP
+     */
     function addLiquidity(
-        uint256 amount,
-        address recipient
-    ) external override returns (ChromaticLPReceipt memory receipt) {
+        uint256 /* amount */,
+        address /* recipient */
+    ) external override returns (ChromaticLPReceipt memory /* receipt */) {
         _fallback();
     }
 
+    /**
+     * @inheritdoc IChromaticLP
+     */
     function removeLiquidity(
-        uint256 lpTokenAmount,
-        address recipient
-    ) external override returns (ChromaticLPReceipt memory receipt) {
+        uint256 /* lpTokenAmount */,
+        address /* recipient */
+    ) external override returns (ChromaticLPReceipt memory /* receipt */) {
         _fallback();
     }
 
-    function settle(uint256 receiptId) external override returns (bool) {
-        // return _settle(receiptId);
+    /**
+     * @inheritdoc IChromaticLP
+     */
+    function settle(uint256 /* receiptId */) external override returns (bool) {
         _fallback();
     }
 
+    /**
+     * @inheritdoc IChromaticLP
+     */
     function resolveSettle(
         uint256 receiptId
     ) public view override(IChromaticLP) returns (bool, bytes memory) {
         return _resolveSettle(receiptId, this.settleTask);
     }
 
+    /**
+     * @inheritdoc IChromaticLP
+     */
     function resolveRebalance() external view override(IChromaticLP) returns (bool, bytes memory) {
         return _resolveRebalance(this.rebalance);
     }
@@ -124,10 +131,6 @@ contract ChromaticLP is ChromaticLPBase, Proxy, IChromaticLP, IChromaticLiquidit
         return s_state.receipts[receiptId];
     }
 
-    function decimals() public view virtual override returns (uint8) {
-        return s_config.market.settlementToken().decimals();
-    }
-
     /**
      * @inheritdoc IChromaticLiquidityCallback
      * @dev not implemented
@@ -141,11 +144,11 @@ contract ChromaticLP is ChromaticLPBase, Proxy, IChromaticLP, IChromaticLiquidit
      * @dev not implemented
      */
     function claimLiquidityCallback(
-        uint256,
-        int16,
-        uint256,
-        uint256,
-        bytes calldata
+        uint256 /* receiptId */,
+        int16 /* feeRate */,
+        uint256 /* depositedAmount */,
+        uint256 /* mintedCLBTokenAmount */,
+        bytes calldata /* data */
     ) external pure override {
         revert OnlyBatchCall();
     }
@@ -154,7 +157,11 @@ contract ChromaticLP is ChromaticLPBase, Proxy, IChromaticLP, IChromaticLiquidit
      * @inheritdoc IChromaticLiquidityCallback
      * @dev not implemented
      */
-    function removeLiquidityCallback(address, uint256, bytes calldata) external pure override {
+    function removeLiquidityCallback(
+        address /* clbToken */,
+        uint256 /* clbTokenId */,
+        bytes calldata /* data */
+    ) external pure override {
         revert OnlyBatchCall();
     }
 
@@ -163,19 +170,63 @@ contract ChromaticLP is ChromaticLPBase, Proxy, IChromaticLP, IChromaticLiquidit
      * @dev not implemented
      */
     function withdrawLiquidityCallback(
-        uint256,
-        int16,
-        uint256,
-        uint256,
-        bytes calldata
+        uint256 /* receiptId */,
+        int16 /* feeRate */,
+        uint256 /* withdrawnAmount */,
+        uint256 /* burnedCLBTokenAmount */,
+        bytes calldata /* data */
     ) external pure override {
         revert OnlyBatchCall();
     }
 
+    /**
+     * @inheritdoc IChromaticLiquidityCallback
+     * @dev not implemented
+     */
     function addLiquidityBatchCallback(
-        address _settlementToken,
-        address vault,
-        bytes calldata data
+        address /* settlementToken */,
+        address /* vault */,
+        bytes calldata /* data */
+    ) external override {
+        _fallback();
+    }
+
+    /**
+     * @inheritdoc IChromaticLiquidityCallback
+     * @dev not implemented
+     */
+    function claimLiquidityBatchCallback(
+        uint256[] calldata /* receiptIds */,
+        int16[] calldata /* feeRates */,
+        uint256[] calldata /* depositedAmounts */,
+        uint256[] calldata /* mintedCLBTokenAmounts */,
+        bytes calldata /* data */
+    ) external override {
+        _fallback();
+    }
+
+    /**
+     * @inheritdoc IChromaticLiquidityCallback
+     * @dev not implemented
+     */
+    function removeLiquidityBatchCallback(
+        address /* clbToken */,
+        uint256[] calldata /* clbTokenIds */,
+        bytes calldata /* data */
+    ) external override {
+        _fallback();
+    }
+
+    /**
+     * @inheritdoc IChromaticLiquidityCallback
+     * @dev not implemented
+     */
+    function withdrawLiquidityBatchCallback(
+        uint256[] calldata /* receiptIds */,
+        int16[] calldata /* feeRates */,
+        uint256[] calldata /* withdrawnAmounts */,
+        uint256[] calldata /* burnedCLBTokenAmounts */,
+        bytes calldata /* data */
     ) external override {
         _fallback();
     }
@@ -211,44 +262,21 @@ contract ChromaticLP is ChromaticLPBase, Proxy, IChromaticLP, IChromaticLiquidit
      */
     function supportsInterface(bytes4 interfaceID) external pure returns (bool) {
         return
-            // super.supportsInterface(interfaceID) ||
             interfaceID == this.supportsInterface.selector || // ERC165
             interfaceID == this.onERC1155Received.selector ^ this.onERC1155BatchReceived.selector; // IERC1155Receiver
     }
 
-    function claimLiquidityBatchCallback(
-        uint256[] calldata /* receiptIds */,
-        int16[] calldata /* feeRates */,
-        uint256[] calldata /* depositedAmounts */,
-        uint256[] calldata /* mintedCLBTokenAmounts */,
-        bytes calldata data
-    ) external override {
-        _fallback();
-    }
-
-    function removeLiquidityBatchCallback(
-        address clbToken,
-        uint256[] calldata clbTokenIds,
-        bytes calldata data
-    ) external override {
-        _fallback();
-    }
-
-    function withdrawLiquidityBatchCallback(
-        uint256[] calldata receiptIds,
-        int16[] calldata feeRates,
-        uint256[] calldata withdrawnAmounts,
-        uint256[] calldata burnedCLBTokenAmounts,
-        bytes calldata data
-    ) external override {
-        _fallback();
-    }
-
+    /**
+     * @dev called by Keeper only
+     */
     function rebalance() public {
         _fallback();
     }
 
-    function settleTask(uint256 receiptId) external {
+    /**
+     * @dev called by Keeper only
+     */
+    function settleTask(uint256 /* receiptId */) external {
         _fallback();
     }
 }
