110c794c04cf2dc4043c5d684f6e5f32e4465ddf
kayden-fi <kayden.choi@quarkonix.com>
2023-09-19 11:16:50 +0900

    fix: deploy command
     optimize imports

--- contracts/core/base/gelato/AutomateReady.sol
+++ contracts/core/base/gelato/AutomateReady.sol
@@ -2,8 +2,7 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {IOpsProxyFactory, ITaskTreasuryUpgradable, ModuleData} from "@chromatic-protocol/contracts/core/base/gelato/Types.sol";
-import {IAutomate} from "@chromatic-protocol/contracts/core/base/gelato/Types.sol";
+import {IAutomate, IOpsProxyFactory, ITaskTreasuryUpgradable, ModuleData} from "@chromatic-protocol/contracts/core/base/gelato/Types.sol";
 
 /**
  * @dev Inherit this contract to allow your smart contract to
