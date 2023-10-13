af34fff348bcab5559bd70ac9a714de4018b06ca
Aryan Malik <88618913+Aryan9592@users.noreply.github.com>
2023-10-13 23:15:29 +0530

    Update PythFeedOracle.sol

--- contracts/oracle/PythFeedOracle.sol
+++ contracts/oracle/PythFeedOracle.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity >=0.8.0 <0.9.0;
 
-import "@openzeppelin/contracts/utils/math/SafeCast.sol";
 import "./interfaces/IOracleProvider.sol";
 import "@pythnetwork/pyth-sdk-solidity/AbstractPyth.sol";
 import {SignedMath} from "@openzeppelin/contracts/utils/math/SignedMath.sol";
