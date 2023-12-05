905b6574ffd5f408e644425bbd7acb37e2c068ef
daniel-fi <daniel.fi@quarkonix.com>
2023-12-05 13:31:33 +0900

    fix: handle position events Fixes #701

--- subgraph/src/chromatic-account.ts
+++ subgraph/src/chromatic-account.ts
@@ -1,9 +1,10 @@
 import { Address, BigInt } from '@graphprotocol/graph-ts'
 import {
+  ClaimPosition as ClaimPositionEvent,
   ClosePosition as ClosePositionEvent,
   OpenPosition as OpenPositionEvent
 } from '../generated/IChromaticAccount/IChromaticAccount'
-import { ClosedPosition, Position } from '../generated/schema'
+import { ClaimedPosition, ClosedPosition, Position } from '../generated/schema'
 
 function id(marketAddress: Address, positionId: BigInt): string {
   return marketAddress.toHex() + '/' + positionId.toString()
@@ -30,6 +31,22 @@ export function handleClosePosition(event: ClosePositionEvent): void {
     entity.position = position.id
     entity.closeVersion = event.params.closeVersion
     entity.closeTimestamp = event.params.closeTimestamp
+
+    entity.save()
+  }
+}
+
+export function handleClaimPosition(event: ClaimPositionEvent): void {
+  let position = Position.load(id(event.params.marketAddress, event.params.positionId))
+  if (position) {
+    let entity = new ClaimedPosition(position.id)
+    entity.position = position.id
+    entity.entryPrice = event.params.entryPrice
+    entity.exitPrice = event.params.exitPrice
+    entity.realizedPnl = event.params.realizedPnl
+    entity.interest = event.params.interest
+    entity.cause = event.params.cause.toString()
+    entity.blockTimestamp = event.block.timestamp
 
     entity.save()
   }
